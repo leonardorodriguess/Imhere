9601902bbe02f8ba6c4a30a268a9375940c9e1a3
Leonardo <leo-feitosa@live.com>
2023-10-08 15:31:16 -0300

    feat: use scroll on screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,36 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
 import { styles } from './styles';
 import { Participant } from '../../components/Participant';
+import React from 'react';
 
 export default function Home() {
+  const participants = [
+    'Leonardo',
+    'Rodrigo',
+    'Vini',
+    'Diego',
+    'Biro',
+    'Ana',
+    'Isa',
+    'Jack',
+    'Maicon',
+    'João',
+  ];
 
   function handleParticipantAdd() {
-    console.log("Você clicou no botão de Adicionar!")
+    console.log('Você clicou no botão de Adicionar!');
   }
-  
+
+  function handleParticipantRemove(name: string) {
+    console.log(`Você clicou em remover o participante ${name}`);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
@@ -15,22 +38,26 @@ export default function Home() {
       <Text style={styles.eventDate}>Domingo, 1 de Outubro de 2023.</Text>
 
       <View style={styles.form}>
-        <TextInput 
+        <TextInput
           style={styles.input}
           placeholder="Nome do participante"
-          placeholderTextColor={"#6B6B6B"}
-          keyboardType={"email-address"}
+          placeholderTextColor={'#6B6B6B'}
+          keyboardType={'email-address'}
         />
 
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
           <Text style={styles.buttonText}> + </Text>
         </TouchableOpacity>
-
       </View>
 
-      <Participant name="Leonardo"/>
-      <Participant name="Diego"/>
-      <Participant name="Rodrigo"/>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {participants.map((participant) => (
+          <Participant
+            key={participant}
+            name={participant}
+          />
+        ))}
+      </ScrollView>
     </View>
   );
 }
