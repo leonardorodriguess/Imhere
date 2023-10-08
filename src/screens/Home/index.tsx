7d3d30b0163488aeb1dc2806bcd8fb87690a2dee
Leonardo <leo-feitosa@live.com>
2023-10-08 15:47:59 -0300

    feat: use flatlist on screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,7 +3,7 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-  ScrollView,
+  FlatList,
 } from 'react-native';
 import { styles } from './styles';
 import { Participant } from '../../components/Participant';
@@ -50,14 +50,22 @@ export default function Home() {
         </TouchableOpacity>
       </View>
 
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {participants.map((participant) => (
+      <FlatList
+        data={participants}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
           <Participant
-            key={participant}
-            name={participant}
+            key={item}
+            name={item}
           />
-        ))}
-      </ScrollView>
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.
+          </Text>
+        )}
+      />
     </View>
   );
 }
